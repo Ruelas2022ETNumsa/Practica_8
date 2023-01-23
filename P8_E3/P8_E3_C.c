b0a92ab07dc30f268223eae712bc74e18574a373
Ruelas_Mijahel <98992663+Ruelas2022ETNumsa@users.noreply.github.com>
2023-01-23 17:33:14 -0400

    Update P8_E3_C.c

--- P8_E3/P8_E3_C.c
+++ P8_E3/P8_E3_C.c
@@ -1,12 +1,24 @@
-#include<stdio.h>
+// Mostrar en la pantalla un triangulo invertivo 
+// cuya altura sea igual al nro ingrasado por teclado
 
+// librerias
+#include<stdio.h>
+#include<conio.h>
+
+// funcion principal
 int main() {
+	// declaracion de variables
 	int i,j,n,var_aux;
+	// enunciado y peticion de datos
+	printf("\n\t\t* EJERCICIO 3 *\n");
+	printf("\n\tIngrese un numero entero :");
+	printf("\n\n\t\t n = ");
+	scanf("%d",&n);
 	printf("\n");
-	printf("\n");
-	n = 6;
 	var_aux = 0;
+	// Comienza el ciclo for
 	for (i=1;i<=n;i++){
+		printf("\t\t");
 		for (j=1;j<=2*n-1-var_aux;j++){
 			if (i<=j) {
 				printf(" *");
@@ -19,7 +31,6 @@ int main() {
 		}
 		printf("\n");
 	}
-	printf("\n");
-	printf("\n");
-	return 0;
+	printf("\n\n\tTerminando el programa");
+	getch();
 }
