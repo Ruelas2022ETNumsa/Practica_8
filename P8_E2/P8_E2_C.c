c2bca8792aebd50ab4261fa323ae7f68d6956ffe
Ruelas_Mijahel <98992663+Ruelas2022ETNumsa@users.noreply.github.com>
2023-01-23 17:27:22 -0400

    Update P8_E2_C.c

--- P8_E2/P8_E2_C.c
+++ P8_E2/P8_E2_C.c
@@ -5,21 +5,28 @@
 // cuando esta llegue a ser igual a la columna, y
 // apartir de ese punto reducir su valor hasta 0
 
+// Librerias
 #include<stdio.h>
 #include<conio.h>
-
+// Funcion principal
 main(){
+	// Declaracion de variables
 	int n,i,j,var_aux;
+	// Pericion de datos y titulo
+	printf("\n\t\t* EJERCICIO 2 *\n");
 	printf("\n\tIngrese un numero entero :");
 	printf("\n\n\t\t n = ");
 	scanf("%d",&n);
-	
+	printf("\n");
+	// Comienza el ciclo for
 	for(i=1 ; i<=n ; i++){
+		printf("\t\t");// Espacios para que no este pegada a un costado de la pantalla
 		for(j=1 ; j<=2*n-1 ; j++){
+			// Condicional para la 1ra parte del triangulo ( Hasta la diagonal de la matriz )
 			if( i>=j ){
 				printf(" %d",j);
 				var_aux = j-1;
-			}else if(var_aux!=0){
+			}else if(var_aux!=0){// Condiconal para mostrar la 2da parte del triangulo
 				do{
 					printf(" %d",var_aux);
 					var_aux--;
